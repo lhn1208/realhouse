1c78af9ee0e7c122b2216c95fda2ac8a280c5039
DESKTOP-ERBM92G\HANA <lhn1208@naver.com>
2018-12-25 23:37:42 +0900

    no message

--- src/js/sub.js
+++ src/js/sub.js
@@ -171,4 +171,55 @@ $(document).ready(function(){
         $(".all_offer.recomm_offer .slide_box:nth-of-type(4) .box_inner").css("bottom","120px");
     }
 
+    //평점
+        var $grade_list=$('.star_grade li');
+        /* 1. Visualizing things on Hover - See next part for action on click */
+         $grade_list.on('mouseover', function(){
+          var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+         
+          // Now highlight all the stars that's not after the current hovered star
+          $(this).parent().children('li.star').each(function(e){
+            if (e < onStar) {
+              $(this).addClass('hover');
+            }
+            else {
+              $(this).removeClass('hover');
+            }
+          });
+          
+        }).on('mouseout', function(){
+          $(this).parent().children('li.star').each(function(e){
+            $(this).removeClass('hover');
+          });
+        });
+        
+        
+        /* 2. Action to perform on click */
+         $grade_list.on('click', function(){
+          var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+          var stars = $(this).parent().children('li.star');
+          
+          for (i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('selected');
+          }
+          
+          for (i = 0; i < onStar; i++) {
+            $(stars[i]).addClass('selected');
+          }
+          
+          // JUST RESPONSE (Not needed)
+          var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+          var msg = "";
+          if (ratingValue > 1) {
+              msg = "Thanks! You rated this " + ratingValue + " stars.";
+          }
+          else {
+              msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+          }
+        });
+        
+        
+      
+      
+    
 });
